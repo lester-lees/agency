e1f19505b0af610fa9fd12ec11697ebf32ac3ff3
jimmy_lees <346994202@qq.com>
2019-04-19 13:09:16 +0800

    axios

--- src/components/RouteLines.vue
+++ src/components/RouteLines.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div>{{ info }}</div>
+    <ul>
+        <li v-for="(r) in routes" v-bind:key="r.line_code">
+            {{ r.on_station_name }} , {{ r.off_station_name }}
+        </li>
+    </ul>
   </div>
 </template>
 
@@ -11,13 +15,21 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      info: null
+      routes: null
     }
   },
   mounted () {
-    this.$http
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    /*
+    this.$http({url: 'http://192.168.0.187:8069/agency/api/search_line/',
+      method: 'post',
+      data: {first: 1, second: 2}})
       .then(response => (this.info = response))
+    */
+    this.$http.post('http://192.168.0.187:8069/agency/api/search_line/', {first: 1, second: 2})
+      .then(response => {
+        // console.info(response.data.result.cities)
+        this.routes = response.data.result.cities
+      })
   }
 }
 </script>
