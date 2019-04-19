cc4121dc3347342ef9d8326587090b8f6eabf727
jimmy_lees <346994202@qq.com>
2019-04-19 17:24:53 +0800

    roughtly

--- src/components/RouteLines.vue
+++ src/components/RouteLines.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <ul>
-        <li v-for="(r) in routes" v-bind:key="r.line_code">
-            {{ r.on_station_name }} , {{ r.off_station_name }}
-        </li>
-    </ul>
+
+  <div class="main_content layout" >
+    <h1>{{ title }}</h1>
+    <Layout>
+        <div class="content">
+            选择路线：
+            <Select id="route" style="width:630px" size="large" filterable placeholder="请选择线路" v-on:change="route_change" v-model="current_route">
+                <Option v-for="(r) in routes" v-bind:key="r.line_code" v-bind:value="r.line_code">
+                    {{ r.on_station_name }} => {{ r.off_station_name }}
+                </Option>
+            </Select>
+            <br/>
+            <br/>
+            <strong>您选择的线路： {{ current_route }}</strong>
+            <br/><br/>
+            选择发车时间：
+            <DatePicker type="date" placeholder="选择日期" style="width: 150px" v-model="departure_date"></DatePicker>
+            <Divider v-if="bus.length>0"/>
+            <div v-if="bus.length>0">
+                <div style="" v-for="b in bus" v-bind:key="b.busNo">                  
+                    <a >
+                        <Icon type="ios-loop-strong"></Icon>
+                        <p slot="title">
+                            <Icon type="ios-film-outline"></Icon>
+                            班次：{{ b.busNo }}, 发车时间：{{ b.departureTime }}， 车辆类型：{{ b.vehicle_type }}, 剩余票数：{{ b.surplusTicket }} 张, 价格：{{ b.price }}元
+                        </p>
+                    </a>                    
+                </div>
+            </div>
+            <Divider />
+            <div style="margin-bottom:10px; width:630px" id="tickets_number">
+                购票数量： <Input type="number" v-model="tickets" style="width: 120px;" />
+            </div>
+            <Button class="submit" type="primary" shape="circle">购买</Button>
+        </div>
+    </Layout>
   </div>
 </template>
 
@@ -14,8 +43,17 @@ export default {
   name: 'routelines',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      routes: null
+      title: '深港通跨境-代理售票',
+      routes: null,
+      tickets: 1,
+      departure_date: (new Date()).toLocaleDateString(),
+      current_route: '',
+      bus: []
+    }
+  },
+  methods: {
+    route_change (event) {
+      console.info('route_change')
     }
   },
   mounted () {
@@ -30,11 +68,29 @@ export default {
         // console.info(response.data.result.cities)
         this.routes = response.data.result.cities
       })
+  },
+  watch: {
+    current_route: function (val, oldVal) {
+      console.info(val)
+      let r = this.routes.filter(i => {
+        return i.line_code === val
+      })
+      let departure_date = this.departure_date.toLocaleDateString()
+      let url = 'http://192.168.0.187:8069/agency/api/search_bus_info/'+val
+      r = r[0]
+      this.$http.post(url, {line_code: val, off_station_code: r.off_station_code, 
+        on_station_code: r.on_station_code, departure_date: departure_date, 
+        vehicle_type: r.vehicle_type, route_type: r.route_type}).then(res => {
+          this.bus = res.data.result.bus
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#route, #tickets_number{
+    margin-top:20px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.prototype.$http = axios
+Vue.use(iView)
 
 Vue.config.productionTip = false
 
