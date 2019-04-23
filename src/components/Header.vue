511bbf7e9beb926ff3a1b9533c43157eb38b14ca
jimmy_lees <346994202@qq.com>
2019-04-23 14:51:47 +0800

    0423

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,58 @@
 <template>
-    <nav>
+    <!-- <nav>
         <ul>
             <li class="pull-left">
                 <router-link to="/routes">代理购票</router-link>
-                <router-link to="/helloworld">Hello World</router-link>
+                <router-link to="/report">销售报表</router-link>
+                <router-link to="/report">佣金返还</router-link>
+                <router-link to="/report">订单明细</router-link>
             </li>
             <li class="pull-right">
                 <router-link to="/login">登录</router-link>
             </li>
         </ul>        
-    </nav>
+    </nav> -->
+
+    <Menu mode="horizontal" :theme="theme1" active-name="1" @on-select="onMenuSelect">
+        <MenuItem name="routes">
+            <Icon type="ios-paper" />
+            线路查询
+        </MenuItem>
+        <MenuItem name="2">
+            <Icon type="ios-people" />
+            代理购票
+        </MenuItem>
+        <Submenu name="report">
+            <template slot="title">
+                <Icon type="ios-stats" />
+                报表分析
+            </template>
+            <MenuItem name="report-1">订单明细</MenuItem>
+            <MenuItem name="report--2">销售报表</MenuItem>
+            <MenuItem name="report--3">佣金返还</MenuItem>
+        </Submenu>
+        <MenuItem name="login">
+            <Icon type="ios-construct" />
+            用户登录
+            <router-link to="/login">用户登录</router-link>
+        </MenuItem>        
+    </Menu> 
+   
 </template>
 
 <script>
 export default{
-  name: 'myheader'
+  name: 'myheader',
+  methods:{  
+    onMenuSelect:function(name){  
+        console.log(name," is clicked!");  
+        if(name==="login"){
+            this.$router.push('/login')  
+        }else if( name === 'routes') {
+            this.$router.push('/routes') 
+        } 
+    }  
+}    
 }
 </script>
 
